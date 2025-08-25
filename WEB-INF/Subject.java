cbdcc42dccece4f8680772cb45fad44b8019fe7f
kobayashi <konata.t@gmail.com>
2025-08-25 13:59:02 +0900

    feat: トップページを追加

---
+++ WEB-INF/Subject.java
@@ -0,0 +1,21 @@
+package com;
+import java.sql.*;
+
+// DBの科目情報を格納する
+public class Subject
+{
+    public int id;
+    public String name;
+
+    // コンストラクタ
+    public Subject(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // コンストラクタ (SQLリザルト版)
+    public Subject(ResultSet results) throws Exception {
+        id = results.getInt("id");
+        name = results.getString("name");
+    }
+}
