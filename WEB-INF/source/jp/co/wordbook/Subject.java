c9f863b442349bdfb3f2b57da3792a843b7f94fb
kobayashi <konata.t@gmail.com>
2025-09-02 21:00:58 +0900

    ServletSupportクラスを追加

--- WEB-INF/source/jp/co/wordbook/Subject.java
+++ WEB-INF/source/jp/co/wordbook/Subject.java
@@ -1,5 +1,6 @@
 package jp.co.wordbook;
 import java.sql.*;
+import java.util.*;
 
 // DBの科目情報を格納する
 public class Subject
@@ -14,8 +15,26 @@ public class Subject
     }
 
     // コンストラクタ (SQLリザルト版)
-    public Subject(ResultSet results) throws Exception {
+    public Subject(ResultSet results) throws SQLException {
         id = results.getInt("id");
         name = results.getString("name");
+    }
+
+
+    // ユーティリティー
+    // SQLのリザルトからSubjectリストを生成
+    public static List<Subject> createList(ResultSet results) {
+
+        List<Subject> subjects = new ArrayList<>();
+
+        try {
+            while (results.next())
+                subjects.add(new Subject(results));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return subjects;
     }
 }
--- WEB-INF/source/jp/co/wordbook/TopServlet.java
+++ WEB-INF/source/jp/co/wordbook/TopServlet.java
@@ -1,55 +1,37 @@
 package jp.co.wordbook;
 
 import java.io.*;
+import java.sql.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.util.*;
-import java.sql.*;
-// import jp.co.wordbook.Subject;
 
 public class TopServlet extends HttpServlet {
     
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException
     {
-        // データベース情報
-        String url = "jdbc:mysql://localhost/mywordbook";
-        String user = "root";
-        String password = "";
+        // データベースへ接続 / 処理実行 / JSPへ送信
+        ServletSupport.accessAndDispatch(
 
-        // JDBCドライバの読み込み (必須)
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            "SELECT * FROM subjects",
+            statement -> {
+                try {
+                    ResultSet results = statement.executeQuery();
+                    
+                    // SQLのリザルトからSubjectリストを生成
+                    List<Subject> subjects = Subject.createList(results);
 
-        // 科目を全て取得
-        String sql = "SELECT * FROM subjects";
-
-        try (
-            // データベースに接続
-            Connection connection = DriverManager.getConnection(url, user, password);
-            PreparedStatement statement = connection.prepareStatement(sql);
-            ResultSet results = statement.executeQuery();
-        ) {
-            List<Subject> subjects = new ArrayList<>();
-
-            // リストに追加
-            while (results.next()) {
-                subjects.add(new Subject(results));
-            }
-
-            // リクエストに追加
-            request.setAttribute("subjects", subjects);
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-        // JSPへ送信
-        String view = "/WEB-INF/views/top.jsp";
-        RequestDispatcher dispatcher = request.getRequestDispatcher(view);
-        dispatcher.forward(request, response);
+                    // リクエストに追加
+                    request.setAttribute("subjects", subjects);
+                    
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            },
+            
+            "/WEB-INF/views/top.jsp",
+            request, response
+        );
     }
 }
